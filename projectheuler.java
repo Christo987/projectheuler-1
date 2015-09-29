63b8ee9b6507ab80ce378c8ad8ff718c55c69742
Christopher <christopher@Christophers-MacBook-Pro.local>
2015-09-29 14:55:37 -0400

    Ssecond commit

--- projectheuler.java
+++ projectheuler.java
@@ -1,13 +1,19 @@
 public class projectheuler {
 	public static void main(String[] args) {
-		int sum = 5;
-		for(int five = 5;five<=1000;five=five+5) {
+		int sum = 0;
+		for(int five = 0;five<1000;five=five+5) {
 			sum = sum+five;
 		}
-		int sumthree =3;
-		for(int three = 3;three<=1000;three=three+3)   {
+		int sumthree =0;
+		for(int three = 0;three<1000;three=three+3)   {
 			sumthree = sumthree+three;
-			}
-		System.out.println(sum+sumthree);
+		}
+		int sumfifteen =0;
+		for(int fifteen = 0;fifteen<1000;fifteen=fifteen+15) {
+			sumfifteen = sumfifteen+fifteen;
+		}
+		System.out.println(sum+sumthree-sumfifteen);
 	}
 }
+
+
