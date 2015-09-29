c8a83f8c192828a37d8bb1677c8c5e26fac18fac
Christopher <christopher@Christophers-MacBook-Pro.local>
2015-09-29 14:31:51 -0400

    First commit

---
+++ projectheuler.java
@@ -0,0 +1,13 @@
+public class projectheuler {
+	public static void main(String[] args) {
+		int sum = 5;
+		for(int five = 5;five<=1000;five=five+5) {
+			sum = sum+five;
+		}
+		int sumthree =3;
+		for(int three = 3;three<=1000;three=three+3)   {
+			sumthree = sumthree+three;
+			}
+		System.out.println(sum+sumthree);
+	}
+}
